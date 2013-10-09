9c80d8d256f121c7d2f9043ae467a0dcdafde707
Hydrax <danielkubitza@hyxmac-5.local>
2013-10-09 22:19:44 +0200

    changed naming.

--- Command/ExportEntitiesCommand.php
+++ Command/ExportEntitiesCommand.php
@@ -1,5 +1,5 @@
 <?php
-namespace HydraxSkarrag\MwbExporterBundle\Command;
+namespace MwbsExporterBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
--- MwbExporterBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace HydraxSkarrag\MwbExporterBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class HydraxSkarragMwbExporterBundle extends Bundle
-{
-}
