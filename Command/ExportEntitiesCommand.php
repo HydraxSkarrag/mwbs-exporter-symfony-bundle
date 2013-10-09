ca0d82a81b7fd4a2d38e83f5a298ea690f86428c
HydraxSkarrag <github@hydrax.org>
2013-10-09 18:24:28 +0200

    Update ExportEntitiesCommand.php

--- Command/ExportEntitiesCommand.php
+++ Command/ExportEntitiesCommand.php
@@ -1,5 +1,5 @@
 <?php
-namespace MwbExporterBundle\Command;
+namespace HydraxSkarrag\MwbExporterBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
