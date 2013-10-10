cb7ce23f2f96acada34c9225e0a1ba080acdb68c
HydraxSkarrag <github@hydrax.org>
2013-10-10 18:29:27 +0200

    mwbs export entities now with bundle configuration instead of config.json file.

--- Command/ExportEntitiesCommand.php
+++ Command/ExportEntitiesCommand.php
@@ -17,10 +17,7 @@ class ExportEntitiesCommand extends ContainerAwareCommand
      */
     protected function configure()
     {
-        $this->setName('mwbs:export-entities')
-            ->setDescription('Exports the MySQLWorkbench to doctrine2-annotation entities')
-            ->addArgument('bundleName', InputArgument::REQUIRED, 'Name of the Bundle')
-            ->addArgument('mwbFile', InputArgument::REQUIRED, 'Mysql-Workbench filename inside Resources/workbench/');
+        $this->setName('mwbs:export-entities')->setDescription('Exports the MySQLWorkbench to doctrine2-annotation entities');
     }
 
     /**
@@ -30,8 +27,6 @@ class ExportEntitiesCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->callMwbExporter($input, $output);
-        
-        $this->callStubGenerate($input, $output);
         
         $this->callSchemaValidation($input, $output);
     }
@@ -43,50 +38,11 @@ class ExportEntitiesCommand extends ContainerAwareCommand
      */
     protected function callMwbExporter(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("\n" . '<info>Export <comment>' . $input->getArgument('mwbFile') . '</comment> workbench:</info>');
+        $output->writeln(sprintf('Dumping all <comment>%s</comment> mysql workbench schemas.', $input->getOption('env')));
         $output->writeln('');
         
-        $kernel = $this->getContainer()->get('kernel');
-        
-        $root_dir = realpath($kernel->getRootDir() . '/../');
-        $config = $kernel->locateResource('@' . $input->getArgument('bundleName') . '/Resources/workbench/' . $input->getArgument('mwbFile') . '.json');
-        $source = $kernel->locateResource('@' . $input->getArgument('bundleName') . '/Resources/workbench/' . $input->getArgument('mwbFile') . '.mwb');
-        $destination = $kernel->locateResource('@' . $input->getArgument('bundleName') . '/Entity/');
-        
-        $command = sprintf('cd %1$s; php bin/export.php --config=%2$s %3$s %4$s', $root_dir, $config, $source, $destination);
-        
-        $returnCode = '';
-        system($command, $returnCode);
-        
-        if ($returnCode != 0) {
-            $output->writeln('<error>export failed...</error>');
-            $output->writeln(sprintf('<comment>%s</comment>', $command));
-        }
-    }
-
-    /**
-     *
-     * @param InputInterface $input            
-     * @param OutputInterface $output            
-     */
-    protected function callStubGenerate(InputInterface $input, OutputInterface $output)
-    {
-        $output->writeln("\n" . '<info>Creating getter and setter:</info>');
-        
-        $command = $this->getApplication()->find('generate:doctrine:entities');
-        $arguments = array(
-            'command' => $command->getName(),
-            'name' => $input->getArgument('bundleName')
-        );
-        $input = new ArrayInput($arguments);
-        
-        $output->writeln(sprintf('call <comment>%s</comment>', $command->getName()));
-        
-        $returnCode = $command->run($input, $output);
-        
-        if ($returnCode != 0) {
-            $output->writeln(sprintf('<error>%s failed...</error>', $command->getName()));
-        }
+        $exporter = $this->getContainer()->get('mwbs_exporter.exporter');
+        $exporter->export($output);
     }
 
     /**
